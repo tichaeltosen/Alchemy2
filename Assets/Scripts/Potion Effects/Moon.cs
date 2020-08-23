00c9ae949da4908fc0a855285f4796928d8e8956
tichaeltosen <mrosen86@gmail.com>
2020-08-23 00:23:53 -0700

    temp moon effect implemented!

---
+++ Assets/Scripts/Potion Effects/Moon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    private string potionReturn;
+    GameObject moonEffect;
+
+    private void OnEnable()
+    {
+
+        potionReturn = CraftRecipe.instance.CraftedPotion();
+        if (potionReturn == "Moon")
+        {
+            UsePotion.Effect += MoonEffect;
+        }
+    }
+
+
+    public void MoonEffect()
+    {
+        Debug.Log("Moon Effect Starting");
+        moonEffect = GameObject.FindGameObjectWithTag("MoonEffect");
+        moonEffect.GetComponent<SpriteRenderer>().enabled = true;
+        UsePotion.Effect -= MoonEffect;
+        Debug.Log("Moon Effect Complete!");
+    }
+
+}
