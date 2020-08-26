4b73fdaabbb967e9abc11e2b9ed0655688026818
David Bernstein <dxbernstein@gmail.com>
2020-08-26 13:28:32 -0400

    Updated Itemproperties.cs
    Changed in-game text display for heavy and electric chest

--- Assets/Scripts/ItemProperties.cs
+++ Assets/Scripts/ItemProperties.cs
@@ -122,7 +122,7 @@ public class ItemProperties : MonoBehaviour
         {
             if (itemName == "Strength Chest")
             {
-                RaycastManager.instance.itemNameText.text = "You Need More Strength To Open";
+                RaycastManager.instance.itemNameText.text = "The lid is stuck. If only you were a little stronger...";
 
             }else if(itemName == "Unlocked")
             {
@@ -130,7 +130,7 @@ public class ItemProperties : MonoBehaviour
 
             }else if(itemName == "Electric Chest")
             {
-                RaycastManager.instance.itemNameText.text = "This Chest Requires Electricity";
+                RaycastManager.instance.itemNameText.text = "The chest seems to require energy of some sort...";
 
             }
         }
